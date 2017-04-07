d45bf0a1d410c97387192c2e99a0f7ce7acda841
SURFACE\ainat <ainatpujolrigual@gmail.com>
2017-04-07 13:20:57 +0200

    login.html finish

--- app/backoffice/home/home.component.ts
+++ app/backoffice/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  templateUrl: '', // cagaremos un home diferente dependiendo si es admin (./homeAdmin.component.html) o usuari (./homeUsuarios.component.html)
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
